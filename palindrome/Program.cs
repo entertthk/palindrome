9a75bc0c6e8fffab1b0b438eb8e626de8e130f5b
entertthk <markkc2@hotmail.com>
2020-01-16 11:51:02 +0200

    palindrome

---
+++ palindrome/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char[] name = new char[1];
+
+            start:
+            for (int i = 0; i < name.Length; i++)
+            {
+                
+                Console.WriteLine("Enter a name:");
+                name[i] = Console.ReadLine();
+            }
+
+            Array.Reverse(name);
+            string backward = new string(name);
+
+
+
+          
+
+
+
+            Console.ReadLine();
+
+            
+
+
+            /*string name;
+            string backwardsName;
+
+            start:
+            Console.WriteLine("Please enter a name: ");
+            name = Console.ReadLine();
+
+            
+            backwardsName = new string(name.Reverse().ToArray());*/
+
+            /*if (name == backwardsName)
+            {
+                Console.WriteLine($"{name} is a palindrome \n");
+            }
+
+            else
+            {
+                Console.WriteLine($"{name} is NOT a palindrome \n");
+            }*/
+
+            goto start;
+
+            
+
+        }
+    }
+}
